39a48fb7be5fdb89dc42dc1d9d0417118068d4d3
Alex Walker <walkernalex@gmail.com>
2025-07-19 19:35:47 -0400

    Update MovieSuggestion.js to extract poster URL

--- src/MovieSuggestion.js
+++ src/MovieSuggestion.js
@@ -9,14 +9,25 @@ function MovieSuggestion() {
   }, []);
 
   return (
-    <div className="movie-suggestions">
-      <h3>Popular Movie Picks to Get You Started</h3>
-      <ul>
-       {movies.slice(0, 5).map((movie) => (
-          <li key={movie.id}>{movie.title}</li>
-        ))}
-      </ul>
+<div className="movie-box">
+  <div className="movie-list">
+    <h3>Popular Movie Picks to Get You Started</h3>
+    <ul>
+      {movies.slice(0, 5).map((movie) => (
+        <li key={movie.id}>{movie.title}</li>
+      ))}
+    </ul>
+  </div>
+
+  {movies[0]?.poster_path && (
+    <div className="movie-poster">
+      <img
+        src={`https://image.tmdb.org/t/p/w342${movies[0].poster_path}`}
+        alt={movies[0].title}
+      />
     </div>
+  )}
+</div>
   );
 }
 
